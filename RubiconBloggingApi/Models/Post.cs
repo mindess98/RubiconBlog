99a5d5573107ebd52df414a4c7259fad912132ce
Imran Polic <imran.polic@authoritypartners.com>
2020-04-15 21:39:39 +0200

    Add Post and Tag models

---
+++ RubiconBloggingApi/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RubiconBloggingApi.Models
+{
+    public class Post
+    {
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public List<Tag> Tags { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
